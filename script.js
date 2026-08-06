f8dc2061bcea7ba2b62fc99cd660c89db140ddbd
tahinurislam4944-png <tahinurislam4944@gmail.com>
2026-08-06 14:40:06 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,148 +1,34 @@
-// ===============================
-// Typing Effect
-// ===============================
+// EmailJS Setup
 
-const titles = [
-    "Data Analyst",
-    "Excel Specialist",
-    "Digital Professional",
-    "English Tutor"
-];
+emailjs.init("eYcu2EHCESlEctEoqMSl");
 
 
-let index = 0;
-let charIndex = 0;
+document
+.getElementById("contact-form")
+.addEventListener("submit", function(e){
 
-const typingElement = document.querySelector(".hero h2");
-
-
-function typeEffect(){
-
-    if(charIndex < titles[index].length){
-
-        typingElement.innerHTML += titles[index].charAt(charIndex);
-
-        charIndex++;
-
-        setTimeout(typeEffect,100);
-
-    }
-
-    else{
-
-        setTimeout(eraseEffect,1500);
-
-    }
-
-}
+    e.preventDefault();
 
 
+    emailjs.sendForm(
+        "service_ez3cw06",
+        "template_53tj39o",
+        this
+    )
 
-function eraseEffect(){
+    .then(function(){
 
-    if(charIndex > 0){
+        alert("Message Sent Successfully!");
 
-        typingElement.innerHTML =
-        titles[index].substring(0,charIndex-1);
+    })
 
-        charIndex--;
+    .catch(function(error){
 
-        setTimeout(eraseEffect,50);
+        alert("Failed to send message!");
 
-    }
+        console.log(error);
 
-    else{
-
-        index++;
-
-        if(index >= titles.length){
-            index = 0;
-        }
-
-
-        setTimeout(typeEffect,500);
-
-    }
-
-}
-
-
-// Start animation
-typingElement.innerHTML="";
-typeEffect();
-
-
-
-
-
-// ===============================
-// Scroll Reveal Animation
-// ===============================
-
-
-const sections = document.querySelectorAll("section");
-
-
-const observer = new IntersectionObserver(
-
-(entries)=>{
-
-
-entries.forEach(entry=>{
-
-
-if(entry.isIntersecting){
-
-entry.target.style.opacity="1";
-
-entry.target.style.transform="translateY(0)";
-
-
-}
+    });
 
 
 });
-
-
-},
-
-{
-
-threshold:0.15
-
-}
-
-);
-
-
-
-
-sections.forEach(section=>{
-
-
-section.style.opacity="0";
-
-section.style.transform="translateY(40px)";
-
-section.style.transition="all 0.8s ease";
-
-
-observer.observe(section);
-
-
-});
-
-
-
-
-
-// ===============================
-// Current Year Footer
-// ===============================
-
-
-const year = new Date().getFullYear();
-
-
-document.querySelector("footer p").innerHTML =
-`© ${year} Tahinur Islam. All Rights Reserved.`;
